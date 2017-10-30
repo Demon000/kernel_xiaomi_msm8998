be7c05189c58b6cb3fdc7b1caf619b8cd6ef5b21
Joel Fernandes <joelaf@google.com>
2017-10-30 21:12:57 +0000

    Revert "sched/core: fix have_sched_energy_data build warning"
    This reverts commit a899b9085c8d5d581b214c24dc707466e8cb479f.
    
    Reverting for now due to suspend warning issues.
    
    Change-Id: I9387297b52552a73d5a253b9e8e4467b366479a5

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -97,9 +97,7 @@
 
 #include "walt.h"
 
-#ifdef CONFIG_SMP
 static bool have_sched_energy_data(void);
-#endif
 
 static atomic_t __su_instances;
 
@@ -235,11 +233,10 @@ static int sched_feat_set(char *cmp)
 				sysctl_sched_features &= ~(1UL << i);
 				sched_feat_disable(i);
 			} else {
-#ifdef CONFIG_SMP
 				if (i == __SCHED_FEAT_ENERGY_AWARE)
 					WARN(!have_sched_energy_data(),
 					     "Missing sched energy data\n");
-#endif
+
 				sysctl_sched_features |= (1UL << i);
 				sched_feat_enable(i);
 			}
