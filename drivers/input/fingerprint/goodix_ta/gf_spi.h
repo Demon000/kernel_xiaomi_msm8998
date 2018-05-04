c54297421936c67b4683f4e0e685c23a504836af
ShevT <shev_t@mail.ru>
2018-05-04 20:31:09 +0300

    Revert "goodix_ta: move netlink messages sending to a worker"
    This reverts commit 45f4b46038f876a5f660931c0e5876b0fb40a36e.

--- drivers/input/fingerprint/goodix_ta/gf_spi.h
+++ drivers/input/fingerprint/goodix_ta/gf_spi.h
@@ -44,15 +44,16 @@ struct gf_dev {
 	struct input_dev *input;
 
 	struct notifier_block notifier;
-	struct work_struct message_work;
-	struct wake_lock fp_wakelock;
-	int message;
+	struct work_struct fb_state_work;
+	int fb_state;
 
 	signed irq_gpio;
 	signed reset_gpio;
 
 	unsigned users;
 	int irq;
+
+	struct wake_lock fp_wakelock;
 };
 
 void sendnlmsg(char *message);
