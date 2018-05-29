6afdbbd96cb7b175d46459ae3b4b4e3f24fb629d
Demon Singur <demonsingur@gmail.com>
2018-05-29 19:44:36 +0000

    drivers: add wakeup logging
    Change-Id: I0fb291b09ea4f7e2d58edbcd006d9e279f765c95

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -30,6 +30,7 @@
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/syscore_ops.h>
 #include <linux/irqchip/msm-mpm-irq.h>
+#include <linux/wakeup_reason.h>
 
 #include <asm/cputype.h>
 #include <asm/exception.h>
@@ -443,6 +444,18 @@ static void gic_show_resume_irq(struct gic_chip_data *gic)
 			name = desc->action->name;
 
 		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
+
+		/* ignore rpm interrupt, since every interrupt in power collapse is coming from rpm */
+		if (irq == 53)
+			continue;
+		/* ignore spmi interrupt, since it will record in __qpnpint_handle_irq() */
+		if (irq == 10)
+			continue;
+		/* ignore gpio interrupt, since it will record in show_resume_gpio_irq() */
+		if (irq == 165)
+			continue;
+
+		log_wakeup_reason(irq);
 	}
 }
 
